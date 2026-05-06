6708ed77846c84acf2b1da72856e4b0bff338208
SinguAkshaya <gayathri.b@clientservertech.com>
2026-05-06 16:07:47 +0530

    your commit message

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,5 @@ import axiosInstance from "./axiosInstance";
 export const updateUserProfile = async (id,data) => {
     const res = await axiosInstance.put(`/users/${id}`,data);
     return res.data;
+    
 };
